e242743c00cae87d200b8c316fa47622d0cedc36
Isaac Kinyanjui Ngugi <117882836+iamisaackn@users.noreply.github.com>
2023-06-11 13:50:42 +0300

    Update index.js

--- calculator/index.js
+++ calculator/index.js
@@ -1,1 +1,18 @@
+// index.js
 
+// Get the display element
+const display = document.querySelector('input[name="display"]');
+
+// Function to handle button clicks
+function handleButtonClick(value) {
+  display.value += value;
+}
+
+// Add event listeners to the buttons
+const buttons = document.querySelectorAll('.calculator input[type="button"]');
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const value = button.value;
+    handleButtonClick(value);
+  });
+});
