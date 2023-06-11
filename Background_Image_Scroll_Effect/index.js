379d2f66a5008f35393bbbcb5070e881cb18ad01
Isaac Kinyanjui Ngugi <117882836+iamisaackn@users.noreply.github.com>
2023-06-11 12:55:10 +0300

    Create index.js

---
+++ Background_Image_Scroll_Effect/index.js
@@ -0,0 +1,10 @@
+const bgImageE1 = document.getElementById ("bg-image");
+
+window.addEventListener("scroll", ()=> {
+    updateImage()
+})
+
+function updateImage() {
+    bgImageE1.style.opacity = 1 - window.pageYOffset / 800;
+    bgImageE1.style.backgroundSize = 150 - window.pageYOffset/12 + "%";
+}
