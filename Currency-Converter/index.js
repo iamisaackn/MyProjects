becdc749a010bf4cba44f8b1c7caaaaf7c4c34e4
Isaac Kinyanjui Ngugi <117882836+iamisaackn@users.noreply.github.com>
2023-06-23 16:31:44 +0300

    Add files via upload

--- Currency-Converter/index.js
+++ Currency-Converter/index.js
@@ -1,3 +1,4 @@
+// Retrieve references to HTML elements
 const currencyFirstEl = document.getElementById("currency-first");
 const currencySecondEl = document.getElementById("currency-second");
 const worthFirstEl = document.getElementById("worth-first");
@@ -6,58 +7,62 @@ const exchangeRateEl = document.getElementById("exchange-rate");
 
 // Fetch currency options from API
 fetch("https://v6.exchangerate-api.com/v6/bb674972d6642a0f38be5058/latest/USD")
-  .then((res) => res.json())
+  .then((res) => res.json()) // Convert response to JSON
   .then((data) => {
-    const currencies = Object.keys(data.conversion_rates);
+    const currencies = Object.keys(data.conversion_rates); // Extract available currencies from the data
 
     // Generate select options for currency-first
     currencies.forEach((currency) => {
-      const option = document.createElement("option");
-      option.value = currency;
-      option.textContent = currency;
-      currencyFirstEl.appendChild(option);
+      const option = document.createElement("option"); // Create a new option element
+      option.value = currency; // Set the option value
+      option.textContent = currency; // Set the option text content
+      currencyFirstEl.appendChild(option); // Append the option to the currency-first select element
     });
 
     // Generate select options for currency-second
     currencies.forEach((currency) => {
-      const option = document.createElement("option");
-      option.value = currency;
-      option.textContent = currency;
-      currencySecondEl.appendChild(option);
+      const option = document.createElement("option"); // Create a new option element
+      option.value = currency; // Set the option value
+      option.textContent = currency; // Set the option text content
+      currencySecondEl.appendChild(option); // Append the option to the currency-second select element
     });
 
     // Update exchange rate and worth on initial load
-    updateRate();
+    updateRate(); // Call the updateRate function to update the exchange rate and worth
   })
   .catch((error) => {
-    console.error("Error fetching currency options:", error);
+    console.error("Error fetching currency options:", error); // Handle errors during currency fetch
   });
 
+// Function to update exchange rate and worth
 function updateRate() {
-  const selectedCurrencyFirst = currencyFirstEl.value;
-  const selectedCurrencySecond = currencySecondEl.value;
+  const selectedCurrencyFirst = currencyFirstEl.value; // Get the selected currency from currency-first select element
+  const selectedCurrencySecond = currencySecondEl.value; // Get the selected currency from currency-second select element
 
   // Fetch exchange rate for selected currencies
   fetch(`https://v6.exchangerate-api.com/v6/bb674972d6642a0f38be5058/latest/${selectedCurrencyFirst}`)
-    .then((res) => res.json())
+    .then((res) => res.json()) // Convert response to JSON
     .then((data) => {
-      const rate = data.conversion_rates[selectedCurrencySecond];
+      const rate = data.conversion_rates[selectedCurrencySecond]; // Get the exchange rate for selected currencies
       if (rate) {
+        // If a valid rate exists, update the exchange rate and worth
         exchangeRateEl.textContent = `1 ${selectedCurrencyFirst} = ${rate} ${selectedCurrencySecond}`;
         worthSecondEl.value = (worthFirstEl.value * rate).toFixed(2);
       } else {
+        // If the rate is invalid, display an error message
         exchangeRateEl.textContent = "Invalid currency selection";
         worthSecondEl.value = "";
       }
     })
     .catch((error) => {
+      // Handle errors during exchange rate fetch
       exchangeRateEl.textContent = "Error fetching exchange rate";
       worthSecondEl.value = "";
       console.error("Error fetching exchange rate:", error);
     });
 }
 
-currencyFirstEl.addEventListener("change", updateRate);
-currencySecondEl.addEventListener("change", updateRate);
-worthFirstEl.addEventListener("input", updateRate);
-
+// Event listeners for currency and worth elements
+currencyFirstEl.addEventListener("change", updateRate); // Listen for changes in currency-first select element
+currencySecondEl.addEventListener("change", updateRate); // Listen for changes in currency-second select element
+worthFirstEl.addEventListener("input", updateRate); // Listen for input changes in worth-first input element
