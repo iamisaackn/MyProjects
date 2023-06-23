34b9252ab54c40a369ff9135503283c2f10970a0
Isaac Kinyanjui Ngugi <117882836+iamisaackn@users.noreply.github.com>
2023-06-23 16:22:16 +0300

    Add files via upload

--- Currency-Converter/index.js
+++ Currency-Converter/index.js
@@ -1,31 +1,63 @@
 const currencyFirstEl = document.getElementById("currency-first");
-const worthFirstEl = document.getElementById("worth-first");
 const currencySecondEl = document.getElementById("currency-second");
+const worthFirstEl = document.getElementById("worth-first");
 const worthSecondEl = document.getElementById("worth-second");
 const exchangeRateEl = document.getElementById("exchange-rate");
 
-updateRate();
+// Fetch currency options from API
+fetch("https://v6.exchangerate-api.com/v6/bb674972d6642a0f38be5058/latest/USD")
+  .then((res) => res.json())
+  .then((data) => {
+    const currencies = Object.keys(data.conversion_rates);
+
+    // Generate select options for currency-first
+    currencies.forEach((currency) => {
+      const option = document.createElement("option");
+      option.value = currency;
+      option.textContent = currency;
+      currencyFirstEl.appendChild(option);
+    });
+
+    // Generate select options for currency-second
+    currencies.forEach((currency) => {
+      const option = document.createElement("option");
+      option.value = currency;
+      option.textContent = currency;
+      currencySecondEl.appendChild(option);
+    });
+
+    // Update exchange rate and worth on initial load
+    updateRate();
+  })
+  .catch((error) => {
+    console.error("Error fetching currency options:", error);
+  });
 
 function updateRate() {
-    fetch(`https://v6.exchangerate-api.com/v6/bb674972d6642a0f38be5058/latest/${currencyFirstEl.value}`)
-        .then((res) => res.json())
-        .then((data) => {
-            const rate = data.conversion_rates[currencySecondEl.value];
-            if (rate) {
-                exchangeRateEl.innerText = `1 ${currencyFirstEl.value} = ${rate} ${currencySecondEl.value}`;
-                worthSecondEl.value = (worthFirstEl.value * rate).toFixed(2);
-            } else {
-                exchangeRateEl.innerText = "Invalid currency selection";
-                worthSecondEl.value = "";
-            }
-        })
-        .catch((error) => {
-            exchangeRateEl.innerText = "Error fetching exchange rate";
-            worthSecondEl.value = "";
-            console.error(error);
-        });
+  const selectedCurrencyFirst = currencyFirstEl.value;
+  const selectedCurrencySecond = currencySecondEl.value;
+
+  // Fetch exchange rate for selected currencies
+  fetch(`https://v6.exchangerate-api.com/v6/bb674972d6642a0f38be5058/latest/${selectedCurrencyFirst}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const rate = data.conversion_rates[selectedCurrencySecond];
+      if (rate) {
+        exchangeRateEl.textContent = `1 ${selectedCurrencyFirst} = ${rate} ${selectedCurrencySecond}`;
+        worthSecondEl.value = (worthFirstEl.value * rate).toFixed(2);
+      } else {
+        exchangeRateEl.textContent = "Invalid currency selection";
+        worthSecondEl.value = "";
+      }
+    })
+    .catch((error) => {
+      exchangeRateEl.textContent = "Error fetching exchange rate";
+      worthSecondEl.value = "";
+      console.error("Error fetching exchange rate:", error);
+    });
 }
 
 currencyFirstEl.addEventListener("change", updateRate);
 currencySecondEl.addEventListener("change", updateRate);
 worthFirstEl.addEventListener("input", updateRate);
+
